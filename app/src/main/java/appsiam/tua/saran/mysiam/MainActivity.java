59e02a3dfb0bf31a4df29af2b25c9cf8e83008b1
Satuaz <uppersoul_01@hotmail.com>
2017-06-18 10:42:00 +0700

    Intent to Register

--- app/src/main/java/appsiam/tua/saran/mysiam/MainActivity.java
+++ app/src/main/java/appsiam/tua/saran/mysiam/MainActivity.java
@@ -1,13 +1,60 @@
 package appsiam.tua.saran.mysiam;
 
+        import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+
+        import static appsiam.tua.saran.mysiam.R.id.btnLogin;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Explicit คือ การประกาศตัวแปร
+    private EditText userEditText, passwordEditText;
+    private TextView textView; //shift+ctrl+enterเป็นตัวช่วยในการ Autocorrect
+    private Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initail View
+        initialView();
+
+        //TextView Controller
+        textViewController();
+
+    }   // Method Main
+
+    private void textViewController() {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Intent to NewRegisterActivity เป็นการสืบทอด
+                Intent intent = new Intent(MainActivity.this, NewRegisterAct.class);
+                startActivity(intent);//ให้มีการเปิดหน้า 2 และมันจะทับกับหน้าแรก
+
+
+
+            }
+        });
     }
-}
+
+    private void initialView() {
+
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+
+    }
+
+}   //Main Class นี่คือ คลาสหลัก
---
+++ app/src/main/java/appsiam/tua/saran/mysiam/NewRegisterAct.java
@@ -0,0 +1,13 @@
+package appsiam.tua.saran.mysiam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewRegisterAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_register);
+    }
+}
